9fa5114fcc96172b2dee588ab6f38849460cf3f9
Serban Iordache <iordache@hotmail.com>
2019-01-10 18:12:02 +0100

    Travis test

--- travis-build.sh
+++ travis-build.sh
@@ -5,7 +5,7 @@ env
 ls -al
 ls -al ..
 mkdir moditect-gradle-plugin
-find . -maxdepth 1 | grep -v moditect-gradle-plugin| xargs -i mv {} ./moditect-gradle-plugin
+find . -maxdepth 1 -not -name . -not -name moditect-gradle-plugin | xargs -i mv {} ./moditect-gradle-plugin
 ls -al
 git clone --depth 1 https://github.com/moditect/moditect.git moditect
 ls -al
