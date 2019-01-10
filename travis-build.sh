03bb9210260f5223532562721f375d2bb420b8a9
Serban Iordache <iordache@hotmail.com>
2019-01-10 17:57:22 +0100

    Travis test

--- travis-build.sh
+++ travis-build.sh
@@ -3,5 +3,9 @@ set -ev
 pwd
 env
 ls -al
-ls-al ..
+ls -al ..
+mkdir moditect-gradle-plugin
+mv !(moditect-gradle-plugin) moditect-gradle-plugin
+ls -al
+git clone --depth 1 https://github.com/moditect/moditect.git moditect
 # ./gradlew --no-daemon -i -s build
